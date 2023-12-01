43021ab5fa6970d252fcfa1af6bad9dd70674990
bredr <davidbrett88@gmail.com>
2023-12-01 20:31:38 +0000

    day1 part2

--- go/day1/main.go
+++ go/day1/main.go
@@ -29,5 +29,34 @@ func main() {
 		part1 += value
 	}
 	fmt.Println("part1 = ", part1)
+	part2 := 0
+	for _, line := range raw {
+		digits := findWithOverlaps(line)
+		first := digits[0]
+		last := digits[len(digits)-1]
+		value, err := strconv.Atoi(first + last)
+		if err != nil {
+			log.Fatal(err)
+		}
+		part2 += value
+	}
+	fmt.Println("part2 = ", part2)
+}
 
+func findWithOverlaps(x string) []string {
+	r := regexp.MustCompile(`^(\d|zero|one|two|three|four|five|six|seven|eight|nine)`)
+	lookup := map[string]string{"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+	var found []string
+	for i := 0; i < len(x); i++ {
+		match := r.FindString(x[i:])
+		if match != "" {
+			if value, ok := lookup[match]; ok {
+				match = value
+			}
+			found = append(found, match)
+
+		}
+	}
+	return found
 }
