f5e314f66e60b3ca681e8637fec38b364fba5cfa
bredr <davidbrett88@gmail.com>
2023-12-03 15:38:17 +0000

    day3 part2

--- go/day3/main.go
+++ go/day3/main.go
@@ -62,4 +62,56 @@ func main() {
 		}
 	}
 	fmt.Println("part1 = ", part1)
+
+	gears := make(map[[2]int][]int)
+	for y, row := range grid {
+		rawNumber := ""
+		adjacentPoints := make(map[[2]int]struct{})
+		for x, v := range row {
+			if strings.ContainsAny(v, "0123456789") {
+				rawNumber += v
+
+				// check all adjacent if part
+				for xx := max(x-1, 0); xx <= min(len(row)-1, x+1); xx++ {
+					for yy := max(y-1, 0); yy <= min(len(grid)-1, y+1); yy++ {
+						if !(xx == x && yy == y) {
+							if strings.Contains(grid[yy][xx], "*") {
+								adjacentPoints[[2]int{xx, yy}] = struct{}{}
+							}
+						}
+					}
+				}
+			} else {
+				if len(adjacentPoints) > 0 && rawNumber != "" {
+					number, err := strconv.Atoi(rawNumber)
+					if err != nil {
+						panic(err)
+					}
+					for point := range adjacentPoints {
+						gears[point] = append(gears[point], number)
+					}
+				}
+				rawNumber = ""
+				adjacentPoints = make(map[[2]int]struct{})
+			}
+		}
+		if len(adjacentPoints) > 0 && rawNumber != "" {
+			number, err := strconv.Atoi(rawNumber)
+			if err != nil {
+				panic(err)
+			}
+			for point := range adjacentPoints {
+				gears[point] = append(gears[point], number)
+			}
+		}
+	}
+
+	part2 := 0
+	for _, parts := range gears {
+		if len(parts) == 2 {
+			part2 += parts[0] * parts[1]
+		}
+	}
+	fmt.Println("part2 = ", part2)
+
 }
