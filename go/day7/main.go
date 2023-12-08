47c2dbc59a12a0b6b0d7d03c06ad711bbff45779
bredr <davidbrett88@gmail.com>
2023-12-08 15:46:59 +0000

    day7 part2

--- go/day7/main.go
+++ go/day7/main.go
@@ -75,6 +75,93 @@ func main() {
 	}
 	fmt.Println("part1 = ", part1)
 
+	scoreMapPart2 := map[string]int{"A": 13, "K": 12, "Q": 11, "J": 0, "T": 9, "9": 8, "8": 7, "7": 6, "6": 5, "5": 4, "4": 3, "3": 2, "2": 1}
+	hands = []Hand{}
+	for _, line := range lines {
+		splitLine := strings.Split(line, " ")
+
+		score, _ := strconv.Atoi(splitLine[1])
+		var cards [5]int
+		for i, card := range strings.Split(splitLine[0], "") {
+			cards[i] = scoreMapPart2[card]
+		}
+		hands = append(hands, Hand{Cards: cards, Score: score})
+	}
+
+	for i, hand := range hands {
+		hands[i].RankingScore = maxScore(hand.Cards)
+	}
+	sort.SliceStable(hands, func(i, j int) bool {
+		if hands[i].RankingScore == hands[j].RankingScore {
+			for i, cardi := range hands[i].Cards {
+				cardj := hands[j].Cards[i]
+				if cardi != cardj {
+					return cardi < cardj
+				}
+			}
+		}
+		return hands[i].RankingScore < hands[j].RankingScore
+	})
+	part2 := 0
+	for i, hand := range hands {
+		part2 += (i + 1) * hand.Score
+	}
+	fmt.Println("part2 = ", part2)
+}
+
+func maxScore(x [5]int) int {
+	maxScore := 0
+	cardValues := make([]int, 13)
+	for idx := range cardValues {
+		cardValues[idx] = idx + 1
+	}
+	scores := []struct {
+		f     func(x [5]int) bool
+		value int
+	}{
+		{isFiveOfAKind, 7},
+		{isFourOfAKind, 6},
+		{isFullHouse, 5},
+		{isThreeOfAKind, 4},
+		{isTwoPair, 3},
+		{isPair, 2},
+		{isHighCard, 1},
+	}
+	jackIdx := []int{}
+	for idx, v := range x {
+		if v == 0 {
+			jackIdx = append(jackIdx, idx)
+		}
+	}
+	if len(jackIdx) == 0 {
+		for _, score := range scores {
+			if score.f(x) && score.value > maxScore {
+				maxScore = score.value
+				break
+			}
+		}
+	} else {
+		np := nextProduct(cardValues, len(jackIdx))
+		for {
+			product := np()
+			if len(product) == 0 {
+				break
+			}
+			xcopy := [5]int{}
+			copy(xcopy[:], x[:])
+			for idx, v := range product {
+				xcopy[jackIdx[idx]] = v
+			}
+			for _, score := range scores {
+				if score.f(xcopy) && score.value > maxScore {
+					maxScore = score.value
+					break
+				}
+			}
+		}
+	}
+
+	return maxScore
 }
 
 func isFiveOfAKind(x [5]int) bool {
@@ -162,4 +249,27 @@ func isHighCard(x [5]int) bool {
 		counts[v]++
 	}
 	return len(counts) == 5
+}
+
+func nextProduct(a []int, r int) func() []int {
+	p := make([]int, r)
+	x := make([]int, len(p))
+	return func() []int {
+		p := p[:len(x)]
+		for i, xi := range x {
+			p[i] = a[xi]
+		}
+		for i := len(x) - 1; i >= 0; i-- {
+			x[i]++
+			if x[i] < len(a) {
+				break
+			}
+			x[i] = 0
+			if i <= 0 {
+				x = x[0:0]
+				break
+			}
+		}
+		return p
+	}
 }
