586532558b6fa37dd08650418eccb842d6162bbf
bredr <davidbrett88@gmail.com>
2023-12-13 16:20:43 +0000

    day13 part2

--- go/day13/main.go
+++ go/day13/main.go
@@ -18,9 +18,15 @@ func main() {
 	part1 := 0
 	for _, block := range blocks {
 		field := parseBlock(block)
-		part1 += findMirror(field)
+		part1 += findMirror(field, 0)
 	}
 	fmt.Println("part1 = ", part1)
+	part2 := 0
+	for _, block := range blocks {
+		field := parseBlock(block)
+		part2 += findMirror(field, 1)
+	}
+	fmt.Println("part2 = ", part2)
 }
 
 type Pos struct {
@@ -46,25 +52,27 @@ func parseBlock(block string) Block {
 	return Block{len(strings.Split(block, "\n")[0]), len(strings.Split(block, "\n")), rocks}
 }
 
-func findMirror(block Block) int {
+func findMirror(block Block, maxMistakes int) int {
 	for mirrorX := 1; mirrorX < block.MaxX; mirrorX++ {
 		delta := mirrorX
 		if mirrorX > block.MaxX/2 {
 			delta = block.MaxX - mirrorX
 		}
-		isMirror := true
+		mistakes := 0
 	xLoop:
 		for y := 0; y < block.MaxY; y++ {
 			for i := 0; i < delta; i++ {
 				_, right := block.Rocks[Pos{mirrorX + i, y}]
 				_, left := block.Rocks[Pos{mirrorX - 1 - i, y}]
 				if left != right {
-					isMirror = false
-					break xLoop
+					mistakes++
+					if mistakes > maxMistakes {
+						break xLoop
+					}
 				}
 			}
 		}
-		if isMirror {
+		if mistakes == maxMistakes {
 			return mirrorX
 		}
 	}
@@ -74,20 +82,22 @@ func findMirror(block Block) int {
 		if mirrorY > block.MaxY/2 {
 			delta = block.MaxY - mirrorY
 		}
-		isMirror := true
+		mistakes := 0
 	yLoop:
 		for x := 0; x < block.MaxX; x++ {
 			for i := 0; i < delta; i++ {
 				_, down := block.Rocks[Pos{x, mirrorY + i}]
 				_, up := block.Rocks[Pos{x, mirrorY - 1 - i}]
 				if up != down {
-					isMirror = false
-					break yLoop
+					mistakes++
+					if mistakes > maxMistakes {
+						break yLoop
+					}
 				}
 			}
 
 		}
-		if isMirror {
+		if mistakes == maxMistakes {
 			return mirrorY * 100
 		}
 	}
