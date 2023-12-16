0b9080fb51e59ec9fbe99f3c6c326c1f424f6dd4
bredr <davidbrett88@gmail.com>
2023-12-16 12:39:02 +0000

    day15 part2

--- go/day15/main.go
+++ go/day15/main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
+
+	"github.com/emirpasic/gods/maps/linkedhashmap"
 )
 
 func main() {
@@ -19,6 +23,30 @@ func main() {
 		part1 += hash(step)
 	}
 	fmt.Println("part1 = ", part1)
+
+	boxes := make(map[int]*linkedhashmap.Map)
+	for _, step := range strings.Split(str, ",") {
+		instruction := getInstruction(step)
+		if _, ok := boxes[instruction.Box]; !ok {
+			boxes[instruction.Box] = linkedhashmap.New()
+		}
+		if instruction.FocalLength != nil {
+			boxes[instruction.Box].Put(instruction.Label, *instruction.FocalLength)
+		} else {
+			boxes[instruction.Box].Remove(instruction.Label)
+		}
+	}
+
+	part2 := 0
+	for i := 0; i < 256; i++ {
+		if lenses, ok := boxes[i]; ok {
+			for j, focalLength := range lenses.Values() {
+				part2 += (i + 1) * (j + 1) * focalLength.(int)
+			}
+		}
+	}
+	fmt.Println("part2 = ", part2)
+
 }
 
 func hash(x string) int {
@@ -29,4 +57,26 @@ func hash(x string) int {
 		currentValue %= 256
 	}
 	return currentValue
+}
+
+type Instruction struct {
+	Box         int
+	Label       string
+	FocalLength *int
+}
+
+func getInstruction(x string) Instruction {
+	lettersRe := regexp.MustCompile(`[a-z]+`)
+	labelString := lettersRe.FindString(x)
+	box := hash(labelString)
+	if strings.ContainsRune(x, '-') {
+		return Instruction{box, labelString, nil}
+	}
+	numberRe := regexp.MustCompile(`\d+`)
+	rawNumber := numberRe.FindString(x)
+	focalLength, err := strconv.Atoi(rawNumber)
+	if err != nil {
+		panic(err)
+	}
+	return Instruction{box, labelString, &focalLength}
 }
--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module aoc23
 
 go 1.21.0
+
+require github.com/emirpasic/gods v1.18.1
